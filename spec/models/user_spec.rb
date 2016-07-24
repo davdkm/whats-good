bdf30ba524a814bbedb099152aa19faaca50738f
davdkm <davd.km@gmail.com>
2016-07-24 14:40:23 -0700

    generated username col for users model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @stub_user = build_stubbed(:post)
+  end
+
+  it "has a name" do
+    expect(@stub_post.title).to eq('MyString')
+  end
+
+  it "has some content" do
+    expect(@stub_post.content).to eq('MyText')
+  end
+
+  it "has a post date time" do
+    expect(@stub_post.post_date).to eq('2016-07-24 13:38:12')
+  end
 end
