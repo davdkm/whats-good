ad067f846ef3aeffa590d786f2675a6b7da57d85
davdkm <davd.km@gmail.com>
2016-07-24 22:24:55 -0700

    added user model associations and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
+  has_many :posts
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :email, uniqueness: true
+  validates_presence_of :username
 end
