e6c0a213955fe069703827a3258d14a00d93657c
davdkm <davd.km@gmail.com>
2016-07-24 23:51:36 -0700

    posts index action added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,10 @@
 class PostsController < ApplicationController
 
+  def index
+    @posts = Post.all
+    render json: @posts
+  end
+  
   def show
     @post = Post.find(params[:id])
     render json: @post
