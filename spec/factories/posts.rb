356dfbbe895e444529b5136d07c06df60e982d7b
davdkm <davd.km@gmail.com>
2016-07-24 23:16:26 -0700

    added post author test

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -3,5 +3,6 @@ FactoryGirl.define do
     title "MyString"
     content "MyText"
     post_date "2016-07-24 13:38:12"
+    association :author, factory: :user, username: "writely"
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   before(:each) do
-    @stub_post = build_stubbed(:post)
+    @stub_post = build_stubbed(:post)    
   end
 
   it "has a title" do
@@ -15,5 +15,9 @@ RSpec.describe Post, type: :model do
 
   it "has a post date time" do
     expect(@stub_post.post_date).to eq('2016-07-24 13:38:12')
+  end
+
+  it "has an author" do
+    expect(@stub_post.author).to be_kind_of(User)
   end
 end
