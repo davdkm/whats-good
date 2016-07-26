699bf05a7332adb0fa16b8a90b9dec1e8d8d7048
davdkm <davd.km@gmail.com>
2016-07-25 18:44:02 -0700

    created home controller

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,3 +3,18 @@ angular
     'ui.router',
     'templates'
   ])
+  .config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function ($stateProvider, $urlRouterProvider) {
+
+      $stateProvider
+        .state('home', {
+          url: '/home',
+          templateUrl: 'home/_home.html',
+          controller: 'HomeController as home'
+        });
+
+      $urlRouterProvider.otherwise('home');
+    }
+  ])
---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+class HomeController < ApplicationController
+
+  def index
+    render 'layouts/application'
+  end
+end
