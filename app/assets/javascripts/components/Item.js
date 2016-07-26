32c84f12f29050167e09c1b5a8968abeea25f4a5
davdkm <davd.km@gmail.com>
2016-07-26 12:55:44 -0700

    created item component

---
+++ app/assets/javascripts/components/Item.js
@@ -0,0 +1,20 @@
+var Item = {
+  restrict: 'E',
+  templateUrl: 'posts/_item.html',
+  bindings: {
+    id: '='
+  },
+  controllerAs: 'item',
+  controller: function (PostsService) {
+    var ctrl = this;
+
+    PostsService.getPost(this.id)
+      .then(function (res) {
+        ctrl.data = res.data;
+      });
+  }
+}
+
+angular
+  .module('app')
+  .component('item', Item);
