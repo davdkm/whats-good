8ec280b212a3c87449a23dbbd909419684ff6396
davdkm <davd.km@gmail.com>
2016-07-26 19:59:25 -0700

    removed console logs

--- app/assets/javascripts/components/Item.js
+++ app/assets/javascripts/components/Item.js
@@ -7,8 +7,7 @@ var Item = {
   controllerAs: 'item',
   controller: function (PostsService) {
     var ctrl = this;
-    console.log(ctrl);
-    console.log(this);
+    
     PostsService.getPost(this.id)
       .then(function (res) {
         ctrl.data = res.data;
