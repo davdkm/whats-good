51831d8a214bacbcc45bd9bf60109aca16bd188e
davdkm <davd.km@gmail.com>
2016-07-28 12:35:40 -0700

    added post create rails action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,25 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
 
   def index
     @posts = Post.all
     render json: @posts
   end
-  
+
   def show
     @post = Post.find(params[:id])
+    render json: @post
+  end
+
+  def create
+    @post = Post.create(post_params.merge(user_id: current_user.id))
     render json: @post
   end
 
   private
 
     def post_params
-      params.require(:post).permit(:id, :title, :author, :content, :post_date)
+      params.require(:post).permit(:id, :title, :author, :content, :post_date, :user_id)
     end
 
 end
