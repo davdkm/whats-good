4237c3683a5d7ab2941a0263ec5741733efda86d
davdkm <davd.km@gmail.com>
2016-07-28 19:10:45 -0700

    added post creation function

--- app/assets/javascripts/services/PostsService.js
+++ app/assets/javascripts/services/PostsService.js
@@ -1,12 +1,19 @@
-function PostsService($http) {
+function PostsService($http, $state) {
   var ctrl = this;
 
   ctrl.getPosts = function () {
     return $http.get('/posts.json');
-  }
+  };
 
   ctrl.getPost = function (id) {
     return $http.get('/posts/' + id + '.json');
+  };
+
+  ctrl.addPost = function (post) {
+    return $http.post('/posts.json', post)
+    .then(function (res) {
+      $state.go('home.post', {id: res.data.id});
+    });
   }
 }
 
