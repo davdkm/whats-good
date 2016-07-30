32c5de6fe5a96e69d58262c9a70167e7a3f58084
davdkm <davd.km@gmail.com>
2016-07-30 16:47:57 -0700

    added post save behavior

--- app/assets/javascripts/controllers/PostController.js
+++ app/assets/javascripts/controllers/PostController.js
@@ -1,13 +1,31 @@
-function PostController(post, Auth) {
+function PostController(post, Auth, PostsService, $stateParams) {
   var ctrl = this;
 
-  ctrl.data = post.data;
+  if (post) {
+    ctrl.data = post.data;
+  }
 
   var currentUser;
 
   Auth.currentUser().then(function (user) {
     ctrl.currentUser = user
   })
+
+  ctrl.editPost = function () {
+    if (ctrl.currentUser.id === ctrl.data.author.id) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  ctrl.save = function () {
+    if ($stateParams.id && ctrl.editPost()) {
+      PostsService.updatePost($stateParams.id, ctrl.data);
+    } else {
+      PostsService.addPost(ctrl.data)
+    }
+  }
 }
 
 angular
