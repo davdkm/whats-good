e5ca9f1b54e61577d6b0bee32aab58b819a30d48
davdkm <davd.km@gmail.com>
2016-07-31 13:24:48 -0700

    post json returns current user vote if logged in

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,4 +1,9 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :content, :created_at, :updated_at
+  attributes :id, :title, :content, :created_at, :updated_at, :votes_count
+  attribute :current_user_voted, if: :current_user
   has_one :author
+
+  def current_user_voted
+    Vote.already_voted(current_user.id, object.id) if current_user
+  end
 end
