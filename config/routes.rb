8bd1b47428840d1debdbfbc3f12f648617b257ef
davdkm <davd.km@gmail.com>
2016-08-05 10:33:01 -0700

    nested reviews under posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'home#index'
-  resources :posts
+  resources :posts do
+    resources :reviews
+  end
+
   resources :users, only: [:show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
