363b476f88469cb05d2f57a8261437d28913dda1
davdkm <davd.km@gmail.com>
2016-08-16 13:58:37 -0700

    added category to serializers

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -3,6 +3,7 @@ class PostSerializer < ActiveModel::Serializer
   has_one :author
   has_one :current_user, if: :current_user
   has_one :user_post_review, if: :current_user
+  has_one :category
 
   def user_post_review
     review = Review.where(post_id: object.id).where(user_id: current_user.id).first
