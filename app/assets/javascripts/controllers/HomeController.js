87a1d30e4e7d56f39973a24c1a24875e582a5f8f
davdkm <davd.km@gmail.com>
2016-08-22 18:09:09 -0700

    added filters to home page

--- app/assets/javascripts/controllers/HomeController.js
+++ app/assets/javascripts/controllers/HomeController.js
@@ -1,11 +1,24 @@
-function HomeController(posts, $state) {
+function HomeController(posts, categories, $scope, $state, $filter) {
   var ctrl = this;
 
   ctrl.posts = posts.data;
-  
+  ctrl.categories = categories.data;
+  ctrl.filter = {};
+  ctrl.filterByCategory = filterByCategory;
+
   ctrl.$state = $state
   ctrl.home = function () {
     return $state.current.name === 'home';
+  }
+
+  function filterByCategory(post) {
+    return ctrl.filter[post.category.name] || noFilter(ctrl.filter);
+  };
+
+  function noFilter(filterObj) {
+    return Object.
+      keys(filterObj).
+      every(function (key) { return !filterObj[key]; });
   }
 }
 
