ec52bb72bd78c5b547f27a882876083cfb59f6d8
davdkm <davd.km@gmail.com>
2016-08-22 21:52:36 -0700

    refactored posts index to seperated controller

--- app/assets/javascripts/controllers/HomeController.js
+++ app/assets/javascripts/controllers/HomeController.js
@@ -1,25 +1,10 @@
-function HomeController(posts, categories, $scope, $state, $filter) {
+function HomeController($scope, $state) {
   var ctrl = this;
 
-  ctrl.posts = posts.data;
-  ctrl.categories = categories.data;
-  ctrl.filter = {};
-  ctrl.filterByCategory = filterByCategory;
-
-  ctrl.$state = $state
-  ctrl.home = function () {
-    return $state.current.name === 'home';
-  }
-
-  function filterByCategory(post) {
-    return ctrl.filter[post.category.name] || noFilter(ctrl.filter);
-  };
-
-  function noFilter(filterObj) {
-    return Object.
-      keys(filterObj).
-      every(function (key) { return !filterObj[key]; });
-  }
+  // ctrl.$state = $state
+  // ctrl.home = function () {
+  //   return $state.current.name === 'home';
+  // }
 }
 
 angular
---
+++ app/assets/javascripts/controllers/PostsController.js
@@ -0,0 +1,22 @@
+function PostsController(posts, categories, $filter) {
+  var ctrl = this;
+
+  ctrl.posts = posts.data;
+  ctrl.categories = categories.data;
+  ctrl.filter = {};
+  ctrl.filterByCategory = filterByCategory;
+
+  function filterByCategory(post) {
+    return ctrl.filter[post.category.name] || noFilter(ctrl.filter);
+  };
+
+  function noFilter(filterObj) {
+    return Object.
+      keys(filterObj).
+      every(function (key) { return !filterObj[key]; });
+  }
+}
+
+angular
+  .module('app')
+  .controller('PostsController', PostsController)
