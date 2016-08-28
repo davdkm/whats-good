86cc51ee7af76ed6b345b3d49d8fd762ad232b26
davdkm <davd.km@gmail.com>
2016-08-27 22:37:54 -0700

    removed unused model

--- app/models/vote.rb
+++
@@ -1,9 +0,0 @@
-class Vote < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :voteable, polymorphic: true, counter_cache: true
-
-  def self.already_voted(user_id, voteable_id)
-    vote = where(user_id: user_id).where(voteable_id: voteable_id)
-    vote.first.voted if vote.present?
-  end
-end
