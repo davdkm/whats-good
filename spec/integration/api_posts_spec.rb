e89eb12972f0753ca80ba8588049d059bfbc0d3f
davdkm <davd.km@gmail.com>
2016-08-28 18:27:50 -0700

    tests get posts index size

---
+++ spec/integration/api_posts_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "Posts API" do
+  it 'sends a list of posts' do
+    FactoryGirl.create_list(:post, 3)
+
+    get '/posts'
+
+    json = JSON.parse(response.body)
+
+    # test for the 200 status-code
+    expect(response).to be_success
+    
+    # check to make sure the right amount of messages are returned
+    expect(json.length).to eq(3)
+  end
+end
