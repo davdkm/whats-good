55b6e395011f9a68fe79c3c5823d11c136372a51
davdkm <davd.km@gmail.com>
2016-10-03 23:03:00 -0700

    Fix gylphicons precompile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require bootstrap
 //= require angular
 //= require angular-messages
 //= require angular-rails-templates
