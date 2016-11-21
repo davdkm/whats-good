488aa7e4608631f56c1b12963400a9d09d472b58
davdkm <davd.km@gmail.com>
2016-11-21 00:16:46 -0800

    Remove unneccessary username from posts

--- db/migrate/20160724203812_create_posts.rb
+++ db/migrate/20160724203812_create_posts.rb
@@ -4,7 +4,6 @@ class CreatePosts < ActiveRecord::Migration
       t.string :title
       t.text :content
       t.references :user, index: true
-      t.string :username, null: false
 
       t.timestamps null: false
     end
