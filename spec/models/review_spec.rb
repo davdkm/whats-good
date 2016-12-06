5c1c040559981f1ae8cf7ab903e234795321d362
davdkm <davd.km@gmail.com>
2016-12-05 20:22:00 -0800

    Test review rating value

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -14,4 +14,9 @@ RSpec.describe Review, type: :model do
     expect(@stub_review.post).to be_kind_of(Post)
   end
 
+  it "has a rating" do
+    expect(@stub_review.rating).to be_kind_of(Fixnum)
+    expect(@stub_review.rating).to eq(4)
+  end
+
 end
